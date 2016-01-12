b8792814447d9d7584a63b3677bdd41b18d35e78
atmiya <atmiya@live.com>
2016-01-12 13:50:04 +0530

    201601121349

--- QuizApp/QuizApp.Domain/Entities/TimeLine.cs
+++ QuizApp/QuizApp.Domain/Entities/TimeLine.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace QuizApp.Domain.Entities
 {
     public class TimeLine
     {
         public string CreatedBy { get; set; }
+
+        [DataType(DataType.DateTime)]
         public DateTime CreatedDate { get; set; }
+
         public string UpdatedBy { get; set; }
+
+        [DataType(DataType.DateTime)]
         public DateTime UpdatedDate { get; set; }
     }
 }
