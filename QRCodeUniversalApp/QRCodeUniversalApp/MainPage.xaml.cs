0a3c70f8ae6c9691f56e0290a674406e555b4946
add <atmiya@live.com>
2016-02-12 00:36:36 +0530

    201602121236

---
+++ QRCodeUniversalApp/QRCodeUniversalApp/MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media.Imaging;
+
+using System.Threading.Tasks;         // Used to implement asynchronous methods
+using Windows.Devices.Enumeration;    // Used to enumerate cameras on the device
+using Windows.Devices.Sensors;        // Orientation sensor is used to rotate the camera preview
+using Windows.Graphics.Display;       // Used to determine the display orientation
+using Windows.Graphics.Imaging;       // Used for encoding captured images
+using Windows.Media;                  // Provides SystemMediaTransportControls
+using Windows.Media.Capture;          // MediaCapture APIs
+using Windows.Media.MediaProperties;  // Used for photo and video encoding
+using Windows.Storage;                // General file I/O
+using Windows.Storage.FileProperties; // Used for image file encoding
+using Windows.Storage.Streams;        // General file I/O
+using Windows.System.Display;         // Used to keep the screen awake during preview and capture
+using Windows.UI.Core;                // Used for updating UI from within async operations
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace QRCodeUniversalApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private MediaCapture _captureManager;
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+
+        }
+
+        private async void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            CameraCaptureUI captureUI = new CameraCaptureUI();
+            captureUI.PhotoSettings.Format = CameraCaptureUIPhotoFormat.Jpeg;
+            captureUI.PhotoSettings.CroppedSizeInPixels = new Size(200, 200);
+
+            StorageFile photo = await captureUI.CaptureFileAsync(CameraCaptureUIMode.Photo);
+
+            if (photo == null)
+            {
+                // User cancelled photo capture
+                return;
+            }
+
+            IRandomAccessStream stream = await photo.OpenAsync(FileAccessMode.Read);
+            BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+            SoftwareBitmap softwareBitmap = await decoder.GetSoftwareBitmapAsync();
+
+            SoftwareBitmap softwareBitmapBGR8 = SoftwareBitmap.Convert(softwareBitmap, BitmapPixelFormat.Bgra8, BitmapAlphaMode.Premultiplied);
+
+            SoftwareBitmapSource bitmapSource = new SoftwareBitmapSource();
+            await bitmapSource.SetBitmapAsync(softwareBitmapBGR8);
+
+            imageControl.Source = bitmapSource;
+        }
+    }
+}
---
+++ QRCode_App/QRCode_App/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Enumeration;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Capture;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace QRCode_App
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private MediaCapture _captureManager;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void InitializeCamera()
+        {
+            var videoDevices = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
+            var rearCamera = videoDevices.First(item => item.EnclosureLocation != null &&
+                                                        item.EnclosureLocation.Panel == Windows.Devices.Enumeration.Panel.Back);
+            _captureManager = new MediaCapture();
+
+            await _captureManager.InitializeAsync(new MediaCaptureInitializationSettings
+            {
+                VideoDeviceId = rearCamera.Id
+            });
+
+            capturePreview.Source = _captureManager;
+            await _captureManager.StartPreviewAsync();
+        }
+    }
+}
