8f05e5dfc1dc73215826b5d971e2b087a9ccf71d
add <atmiya@live.com>
2016-03-14 23:22:13 +0530

    201603141122

--- WCF/EmployeeService/EmployeeService/Employee.cs
+++ WCF/EmployeeService/EmployeeService/Employee.cs
@@ -7,14 +7,16 @@ using System.Runtime.Serialization;
 
 namespace EmployeeService
 {
-    [DataContract]
+    [DataContract(Namespace ="http://atmiya.com/2016/03/14")]
     public class Employee
     {
-        [DataMember]
+        [DataMember(Name ="ID",Order = 1)]
         public int Id { get; set; }
-        [DataMember]
+        [DataMember(Name = "NAME", Order = 2)]
         public string Name { get; set; }
+        [DataMember(Name = "GENDER", Order = 3)]
         public string Gender { get; set; }
+        [DataMember(Name = "DATEOFBIRTH", Order = 4)]
         public DateTime DateOfBirth { get; set; }
     }
 }
