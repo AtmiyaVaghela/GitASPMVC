cf26e16c40785352d702d03d9f2fc55b9f57cb22
atmiya <atmiya@live.com>
2016-06-09 16:33:42 +0530

    201606091633

--- INDMS/INDMS.WebUI/Scripts/AppScript.js
+++ INDMS/INDMS.WebUI/Scripts/AppScript.js
@@ -1,1 +1,25 @@
-﻿
+﻿/// <reference path="../AdminLTE-2.3.0/plugins/jQuery/jQuery-2.1.4.min.js" />
+
+
+
+///GetData Using json
+///Parameter : model,url
+
+function GetData(model,url) {
+    if (obj != '') {
+        jQuery.ajax({
+            url: url,
+            type: 'GET',
+            cache: false,
+            datatype: 'json',
+            data: obj,
+            success: function (result) {
+                return result;
+            },
+            error: function (xhr) {
+                alert("Something seems Wrong");
+                console.log(xhr);
+            }
+        });
+    }
+}
